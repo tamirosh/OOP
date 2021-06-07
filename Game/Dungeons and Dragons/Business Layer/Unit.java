0875087f39c613299a548e1604438bfb3e5ce438
tamirosh <80402047+tamirosh@users.noreply.github.com>
2021-06-07 23:11:55 +0300

    Add files via upload

---
+++ Game/Dungeons and Dragons/Business Layer/Unit.java
@@ -0,0 +1,74 @@
+
+public abstract class Unit extends Tile{
+	
+	//fields:
+	private String Name;
+	private int healthPool;
+	private int healthAmount;
+	private int attackPoints;
+	private int defencePoints;
+	
+	//constructor:
+	public Unit(char typeTile, Position position, String Name, int healthPool, int healthAmount, int attackPoints, int defencePoints) {
+		super(typeTile, position);
+		this.Name = Name;
+		this.healthPool = healthPool;
+		this.healthAmount = healthAmount;
+		this.attackPoints = attackPoints;
+		this.defencePoints = defencePoints;
+	}
+	
+	//methods:
+	public String getName() {
+		return Name;
+	}
+	
+	public int getHealthPool() {
+		return this.healthPool;
+	}
+	
+	public int getHealthAmount() {
+		return this.healthAmount;
+	}
+	
+	public int getAttackPoints() {
+		return this.attackPoints;
+	}
+	
+	public int getDefencePoints() {
+		return this.defencePoints;
+	}
+	
+	public void subtractAttackPoints(int points) {
+		this.attackPoints -= points;
+	}
+	
+	public void addAttackPoints(int points) {
+		this.attackPoints += points;
+	}
+	
+	public void subtractDefencePoints(int points) {
+		this.defencePoints -= points;
+	}
+	
+	public void addDefencePoints(int points) {
+		this.defencePoints += points;
+	}
+	
+	public void subtractHealthAmount(int healthPoints) {
+		this.healthAmount -= healthPoints;
+	}
+	
+	public void addHealthAmount(int healthPoints) {
+		this.healthAmount += healthPoints;
+	}
+	
+	public void updateHealthPool (int newHealthPool) {
+		this.healthPool = newHealthPool;
+	}
+	
+	public abstract String toString();
+	
+	public abstract String attack(Unit unit);
+	
+}
