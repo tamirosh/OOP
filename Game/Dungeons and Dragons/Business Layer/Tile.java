a3857313be2bae65c1b84757a64ed44dc577af17
tamirosh <80402047+tamirosh@users.noreply.github.com>
2021-06-07 22:29:37 +0300

    Add files via upload

---
+++ Game/Dungeons and Dragons/Business Layer/Tile.java
@@ -0,0 +1,18 @@
+
+public abstract class Tile {
+	protected char typeTile;
+	protected Position position;
+	
+	public Tile(char typeTile, Position position) {
+		this.typeTile = typeTile;
+		this.position = position;
+	}
+	
+	public char getTypeTile() {
+		return typeTile;
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+}
