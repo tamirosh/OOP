2db7735bb231a58cca2aedcd685662d9f5d872b0
tamirosh <80402047+tamirosh@users.noreply.github.com>
2021-06-25 22:45:40 +0300

    Add files via upload

---
+++ UI/Game.java
@@ -0,0 +1,17 @@
+package UI;
+
+import java.util.List;
+
+import BusinessLayer.Tile;
+
+public class Game {
+	
+	public Game() {
+		
+	}
+	
+	public GameManager txtToLevel(String txt) {
+		return null;
+	}
+
+}
